fbad863bf6d0986a20324962e735e5b4709f8566
Dominik Meyer <d.meyer99@hotmail.com>
2023-01-16 19:47:37 +0000

    replace init.vim with init.lua work in progress

---
+++ lua/user/options.lua
@@ -0,0 +1,34 @@
+local opt = vim.opt
+
+-- Settings
+opt.backup = false
+opt.cmdheight = 2
+opt.encoding = "utf-8"
+opt.relativenumber = true
+opt.number = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.swapfile = false
+opt.backspace = "2"
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.showtabline = 2
+opt.termguicolors = true
+opt.shiftwidth = 4
+opt.expandtab = false
+opt.listchars = { space = '*', tab = '>-' , trail = '~'}
+opt.list = true
+opt.signcolumn = "yes"
+opt.textwidth = 85
+opt.colorcolumn = "86"
+opt.wrap = true
+opt.linebreak = true
+opt.foldmethod = "syntax"
+opt.foldlevelstart = 1
+opt.shell = "powershell.exe"
+opt.smartindent = true
+opt.splitbelow = true
+opt.splitright = true
+opt.clipboard = "unnamedplus"
+opt.scrolloff = 10
+
